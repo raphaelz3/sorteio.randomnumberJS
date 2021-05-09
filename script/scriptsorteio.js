c4c238cb3c70cb47f85f3ebd2b5d5284e3037313
raphaelz3 <raphael-nascimento@hotmail.com>
2021-05-09 16:34:34 -0300

    favicon img

---
+++ script/scriptsorteio.js
@@ -0,0 +1,43 @@
+//função para gerar um número aleatório. Generate random number
+function gerarnumero(maxvalue, minvalue)
+{
+    var result;
+
+    result = Math.round(Math.random() * (maxvalue - minvalue)) + minvalue;
+    return result;
+}
+//Função inicial, recebe os dados do usuário e faz o tratamento das informações. Initial function, receives the user's data and handles the information.
+function sortear()
+{
+    var min = parseInt(document.getElementById("start").value);
+    var max = parseInt(document.getElementById("end").value);
+    var total = parseInt(document.getElementById("total").value);
+    var sorteado;
+    var finalresult = new Array();
+    var n = 0;
+
+    if(total < max && min < max)
+    {
+        for(var i = 0; i != total; i++)
+        {
+            sorteado = gerarnumero(max, min);
+            while(n < i)
+            {
+                if(sorteado == finalresult[n])
+                {
+                    sorteado = gerarnumero(max, min);
+                    n = 0;
+                }
+                else
+                    n++;
+            }
+            n = 0;
+            finalresult.push(sorteado);
+        }
+        document.getElementById("resultado").innerText = finalresult;
+    }
+    else
+    {
+        document.getElementById("resultado").innerText = "Valores Errados";
+    }
+}
